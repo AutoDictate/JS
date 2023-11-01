7fa7edbcc337c067bd1632b828dc60882f27f525
AutoDictate <jayasurya0206@gmail.com>
2023-11-01 19:23:19 +0530

    Sign UP Form Created

---
+++ JavaScript Learn/literals.js
@@ -0,0 +1,13 @@
+// types of Literals
+
+let name = new String("Surya");
+console.log(name);
+
+let age = new Number(21);
+console.log(age);
+
+let person = new Object();
+console.log(person);
+
+let isAlive = new Boolean(true);
+console.log(name,age,person,isAlive);
